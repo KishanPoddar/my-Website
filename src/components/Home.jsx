2870135d2bd0f85feef3a3aa610bb096efbe2811
kishanpoddar <kishanpoddar2002@gmail.com>
2023-01-18 00:47:53 +0530

    Updated Navbar -- Made Responsive and remove useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import profile from '../assets/pic1.png'
 import profile from "../assets/pic2.png";
 
 const Home = () => {
@@ -11,25 +10,15 @@ const Home = () => {
     ]
 
     return (
-        <section
-            id="home"
-            className="min-h-screen flex py-10 md:flex-row flex-col items-center"
-        >
-            <div className="flex-1 flex items-center justify-center h-full">
-                <img
-                    src={profile}
-                    alt=""
-                    className="md:w-11/12 h-full object-cover"
-                />
-            </div>
-            <div className="flex-1">
+        <section id="home" className="min-h-screen flex justify-center items-center py-10">
+            <div>
                 <div className="md:text-left text-center">
                     <h1 className="md:text-6xl text-2xl md:leading-normal leading-10 text-white font-bold">
-                        <div className="text-cyan-600 md:text-6xl text-5xl">Hello!</div>
+                        <div className="text-cyan-400 md:text-6xl text-5xl">Hello!</div>
                         <div>My name is <span>KISHAN PODDAR</span></div>
                     </h1>
                     <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-2 font-bold text-gray-600">Game Developer | Full Stack Developer | Coder</h4>
-                    <button className="btn-primary mt-8">Contact Me</button>
+                    <button className="btn-primary mt-8 shadow-lg shadow-cyan-400/50">Contact Me</button>
                     <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
                       {
                         social?.map((icon)=>(
@@ -42,6 +31,31 @@ const Home = () => {
                 </div>
             </div>
         </section>
+
+        // <section id="home" className="min-h-screen flex py-10 md:flex-row flex-col items-center">
+        //     <div className="flex-1 flex items-center justify-center h-full">
+        //         <img src={profile} alt="" className="md:w-11/12 h-full object-cover"/>
+        //     </div>
+        //     <div className="flex-1">
+        //         <div className="md:text-left text-center">
+        //             <h1 className="md:text-6xl text-2xl md:leading-normal leading-10 text-white font-bold">
+        //                 <div className="text-cyan-600 md:text-6xl text-5xl">Hello!</div>
+        //                 <div>My name is <span>KISHAN PODDAR</span></div>
+        //             </h1>
+        //             <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-2 font-bold text-gray-600">Game Developer | Full Stack Developer | Coder</h4>
+        //             <button className="btn-primary mt-8">Contact Me</button>
+        //             <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+        //               {
+        //                 social?.map((icon)=>(
+        //                   <div key ={icon} className='text-gray-500 hover:text-white cursor-pointer'>
+        //                     <ion-icon name={icon}></ion-icon>
+        //                   </div>
+        //                 ))
+        //               }
+        //             </div>
+        //         </div>
+        //     </div>
+        // </section>
     );
 };
 
